400bca92721a492c4beef9095bf9d14a5f5c1e78
Uselessbott <charansadimela0@gmail.com>
2026-06-28 22:54:36 +0530

    Capacitor updates and Android fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,34 @@
 import { defineConfig, loadEnv, UserConfigExport, ConfigEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// This is a TypeScript Vite Config file.
-// Comments are retained from both original configurations for clarity.
 export default ({ mode }: ConfigEnv): UserConfigExport => {
-  // Load environment variables and merge them with process.env
-  // You can access Vite specific env variables here like VITE_NAME using process.env.VITE_NAME
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd()),
+  };
 
   return defineConfig({
-    plugins: [react()], // Using React plugin from Vite
+    // Required for Capacitor / Android
+    base: './',
+
+    plugins: [react()],
 
     build: {
-      chunkSizeWarningLimit: 2000, // Control the size before showing a warning for chunk size
-      outDir: 'dist', // Specify your desired output directory
+      outDir: 'dist',
+      chunkSizeWarningLimit: 2000,
       rollupOptions: {
         output: {
           manualChunks: {
-            lodash: ['lodash'], // Manually define chunk for lodash
-            vendor: ['react', 'react-dom'], // Manually define chunk for React and ReactDOM
+            lodash: ['lodash'],
+            vendor: ['react', 'react-dom'],
           },
         },
       },
     },
 
     server: {
-      port: parseInt(process.env.VITE_PORT || '5173'), // Use VITE_PORT from .env.local, default to 5173
-      open: true, // Automatically open the default browser when starting the server
+      port: parseInt(process.env.VITE_PORT || '5173'),
+      open: true,
     },
   });
 };
