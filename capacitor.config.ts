b73f1b726967ef35aad4ddfe2d9d8312a8513046
Uselessbott <charansadimela0@gmail.com>
2026-06-28 22:13:59 +0530

    Add Capacitor Android support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.uselessbott.miruro',
+  appName: 'Miruro',
+  webDir: 'dist'
+};
+
+export default config;
